68a42842f95b858a82b47cac3cc54cfccac5c7fa
Satish <satjonna@cisco.com>
2018-02-05 00:06:46 -0800

    Added Authoriation modules

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -8,6 +8,7 @@ var User = require('../user/User');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
+var VerifyToken = require('./VerifyToken');
 
 router.post('/register', (req, res) => {
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -25,26 +26,48 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.get('/me', function(req, res) {
-    var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+router.get('/me', VerifyToken, function(req, res, next) {
 
-    decoded = jwt.verify(token, config.secret);
+    User.findById(
+        req.userId,
+        {password: 0},
+        (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    jwt.verify(token, config.secret, function(err, decoded) {
-        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-        
-        // res.status(200).send(decoded);
-        User.findById(
-            decoded.id,
-            {password: 0},
-            (err, user) => {
-            if (err) return res.status(500).send("There was a problem finding the user.");
-            if (!user) return res.status(404).send("No user found.");
-
-            res.status(200).send(user);
-        })
+        res.status(200).send(user);
+        // next(user);
     });
+
+    // var token = req.headers['x-access-token'];
+    // if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    // decoded = jwt.verify(token, config.secret);
+    // jwt.verify(token, config.secret, (err, decoded) => {
+    //     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    // });
+});
+
+router.use((user, req, res, next) => {
+    res.status(200).send(user);
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        console.log(req.body);
+        if (err) return res.status(500).send('Error on the server.');
+        if (!user) return res.status(404).send('No user found.');
+
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        if(!passwordIsValid) res.status(401).send({auth: false, token: null});
+
+        var token = jwt.sign({id: user._id}, config.secret, {expiresIn: 86400});
+
+        res.status(200).send({ auth: true, token: token });
+    });
+});
+
+router.get('/logout', function(req, res) {
+    res.status(200).send({ auth: false, token: null });
 });
 
 module.exports = router;
